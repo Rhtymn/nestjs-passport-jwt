17a7b9a6222fa00b4e67bd186e70469ef59c4594
Roihan <hroihan6@gmail.com>
2023-09-10 09:28:27 +0700

    Add example route for using authorization guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { User, UserInfo } from './user/decorators/user.decorator';
+import { AuthorizationGuard } from './auth/guards/authorization.guard';
+import { Roles } from './decorators/roles.decorator';
+import { UserRole } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -11,5 +14,12 @@ export class AppController {
   @Get('profile')
   getProfile(@User() user: UserInfo) {
     return user;
+  }
+
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, AuthorizationGuard)
+  @Get('hello')
+  getHello(): string {
+    return 'Hello World!';
   }
 }
